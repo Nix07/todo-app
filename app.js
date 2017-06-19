3ff3fb9c0364609cbf0ab03ac668a6390567e82f
Nix07 <nikhil07prakash@gmail.com>
2017-06-19 21:18:01 +0530

    Added app.js and started the work with Controllers

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var todoController = require('./Controllers/todoController.js');
+
+var app = express();
+
+//set up template engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('/assets'));
+
+//fire Controllers
+todoController(app);
+
+app.listen(3000);
+console.log('You are listening to port 3000');
